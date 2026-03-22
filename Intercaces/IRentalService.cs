ce9f5f1294c095f2fa191101e39c29394b816c93
Daria Serhiienko <s30246@pjwstk.edu.pl>
2026-03-22 23:57:31 +0100

    Add IRentalService

--- Intercaces/IRentalService.cs
+++ Intercaces/IRentalService.cs
@@ -1,6 +1,20 @@
-namespace APBD_TASK2.Intercaces;
+using APBD_TASK2.Models;
 
-public class IRentalService
+namespace APBD_TASK2.Intercaces
 {
-    
+
+    public interface IRentalService
+    {
+        void AddUser(User user);
+        void AddEquipment(Equipment equipment);
+        List<Equipment> GetAllEquipment();
+        List<Equipment> GetAvailableEquipment();
+
+        bool RentEquipment(int userId, int equipmentId, DateTime returnDate);
+        bool ReturnEquipment(int rentalId);
+        void EquipmentUnavailable(int equipmentId);
+        List<Rental> GetAvtiveRentals(int userId);
+        List<Rental> GetReturnedRentals();
+        void PrintRentals();
+    }
 }
