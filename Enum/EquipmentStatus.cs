96e8d6317fc9a98d55f18529bb1fa5d02570c147
Daria Serhiienko <s30246@pjwstk.edu.pl>
2026-03-22 23:57:18 +0100

    Add EquipmentStatus and userType

--- Enum/EquipmentStatus.cs
+++ Enum/EquipmentStatus.cs
@@ -1,6 +1,10 @@
-namespace APBD_TASK2.Enum;
-
-public class EquipmentStatus
+namespace APBD_TASK2.Enum
 {
-    
+
+    public enum EquipmentStatus
+    {
+        Available,
+        Rented,
+        Unavailable
+    }
 }
--- Enum/UserType.cs
+++ Enum/UserType.cs
@@ -1,6 +1,9 @@
-namespace APBD_TASK2.Enum;
-
-public class UserType
+namespace APBD_TASK2.Enum
 {
-    
+
+    public enum UserType
+    {
+        Student = 1,
+        Employee = 2
+    }
 }
